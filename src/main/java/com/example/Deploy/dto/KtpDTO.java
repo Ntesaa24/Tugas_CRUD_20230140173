a948a81a52e2b87c8498612858f2e65cb2defced
Ntesaa24 <notesa.aldinasari24@gmail.com>
2026-03-10 21:19:34 +0700

    mmembuat KtpDTO.java

---
+++ src/main/java/com/example/Deploy/dto/KtpDTO.java
@@ -0,0 +1,11 @@
+package com.example.Deploy.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
+public class KtpDTO {
+
+
+}
